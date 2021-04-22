7881f4bb373c2cf1d1c201d1e3b5a56447603dd4
LT <leetoplayaz@yandex.ru>
2021-04-22 17:09:26 +0300

    add notify text fix

--- src/PaymentGateways/PaymentGateway.php
+++ src/PaymentGateways/PaymentGateway.php
@@ -33,9 +33,9 @@ class PaymentGateway
     protected $returnUrl;
 
     /**
-     * @var
+     * @var string
      */
-    protected $paymentDescription;
+    protected $paymentDescription = "";
 
     /**
      * @var array
@@ -264,7 +264,7 @@ class PaymentGateway
      */
     protected function getDefaultNotifyMessage(): string
     {
-        return trim("{$this->getNotifyCustomText()} {$this->getPaymentDescription()} - {$this->getAmount()} " . config("cashbox.gateway.currency"));
+        return trim("{$this->getNotifyCustomText()} {$this->getPaymentDescription()} " . config("cashbox.gateway.currency"));
     }
 
     /**
